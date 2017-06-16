e44bcd54884b785970125ce832a6a5a7f923af21
satoshi <satoshidat@gmail.com>
2017-06-16 14:22:36 +0900

    pictureBoxを動くよう変更＆マウスでラベルを移動

--- move-labels/Form1.Designer.cs
+++ move-labels/Form1.Designer.cs
@@ -32,6 +32,8 @@
             this.label1 = new System.Windows.Forms.Label();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -39,7 +41,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Image = global::move_labels.Properties.Resources._5259ab9fc55975208d27f3a22a9ba67d;
-            this.label1.Location = new System.Drawing.Point(130, 151);
+            this.label1.Location = new System.Drawing.Point(52, 19);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(35, 12);
             this.label1.TabIndex = 0;
@@ -53,17 +55,38 @@
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(89, 67);
+            this.pictureBox1.Image = global::move_labels.Properties.Resources._5259ab9fc55975208d27f3a22a9ba67d;
+            this.pictureBox1.Location = new System.Drawing.Point(466, 101);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(94, 51);
+            this.pictureBox1.Size = new System.Drawing.Size(251, 231);
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(302, 145);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(105, 236);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 12);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "label3";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.ClientSize = new System.Drawing.Size(1020, 558);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
@@ -79,6 +102,8 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- move-labels/Form1.cs
+++ move-labels/Form1.cs
@@ -12,8 +12,8 @@ namespace move_labels
 {
     public partial class Form1 : Form
     {
-        int vx = -3;
-        int vy = -3;
+        int vx = -5;
+        int vy = -5;
         public Form1()
         {
             InitializeComponent();
@@ -21,25 +21,47 @@ namespace move_labels
 
         private void timer1_Tick(object sender, EventArgs e)
         {
+
+            // 2次元クラスPoint型の変数cposを宣言
+            Point cpos;
+
+            // cposに、マウスのフォーム座標を取り出す。
+            //// MousePositionにマウス座標のスクリーン左上からのX、Yが入っている。
+            //// PointToClient()を使うと、スクリーン座標を、フォーム座標に変換できる。
+            cpos = PointToClient(MousePosition);
+
+            // ラベルに座標を表示
+            //// 変換したフォーム座標は、cpos.X、cpos.Yで取り出せる。
+            label1.Text = "" + cpos.X + "," + cpos.Y;
+            label2.Text = "" + MousePosition.X + "," + MousePosition.Y;
             
-            label1.Left += vx;
-            label1.Top += vy;
-            if (label1.Left < 0)
+            //新しく作ったlabel3をマウスカーソルの場所に移動
+            //かつマウスカーソルがラベルの中心を目指す
+            //(オフセット、ピボット(pivot)などの概念)
+
+            label3.Left = cpos.X-label3.Width/2;
+            label3.Top = cpos.Y-label3.Height/2;
+            
+
+
+            pictureBox1.Left += vx;
+            pictureBox1.Top += vy;
+            if (pictureBox1.Left < 0)
             {
                 vx =Math.Abs(vx);               
             }
             
-            if(label1.Left>ClientSize.Width-label1.Width)
+            if(pictureBox1.Left>ClientSize.Width-pictureBox1.Width)
             {
                 vx =-Math.Abs(vx);
             }
             
             
-            if (label1.Top < 0)
+            if (pictureBox1.Top < 0)
             {
                 vy =Math.Abs(vy);            
             }
-           if(label1.Top>ClientSize.Height-label1.Height)
+           if(pictureBox1.Top>ClientSize.Height-pictureBox1.Height)
            {
                vy =-Math.Abs(vy);
            }
